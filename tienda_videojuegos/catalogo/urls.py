dbf4d8876b356dbe01eb9117b033560b2ab6b21c
lionel-prats <lionel.prats.c@gmail.com>
2026-01-03 14:48:29 -0300

    Crear un catálogo de productos dinámico con Django

--- tienda_videojuegos/catalogo/urls.py
+++ tienda_videojuegos/catalogo/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.lista_juegos, name="lista_juegos"),
+    # path('contacto/', views.contacto, name="contacto"),
 ]
