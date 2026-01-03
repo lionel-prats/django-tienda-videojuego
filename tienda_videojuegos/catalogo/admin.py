3e43622e9018b5be3d900d4a899c15d4f339e934
lionel-prats <lionel.prats.c@gmail.com>
2026-01-03 17:12:21 -0300

    Cómo funciona el administrador de Django

--- tienda_videojuegos/catalogo/admin.py
+++ tienda_videojuegos/catalogo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Juego
+
+# class CategoriaAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'nombre')
+
+# class ProductoAdmin(admin.ModelAdmin):
+#     exclude = ('creado_en', ) 
+#     list_display = ('id', 'nombre', 'stock', 'creado_en')
+
 
 # Register your models here.
+# admin.site.register(Categoria, CategoriaAdmin)
+# admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Juego)
--- tienda_videojuegos/tienda_videojuegos/settings.py
+++ tienda_videojuegos/tienda_videojuegos/settings.py
@@ -107,7 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+# LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es-arg"
 
 TIME_ZONE = "UTC"
 
